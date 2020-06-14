b7ecf4bd415e0f729efd877dc9e6e911c8c1be45
qq1093814809 <66855958+qq1093814809@users.noreply.github.com>
2020-06-15 00:30:37 +0800

    Add files via upload

---
+++ myoffem/index.js
@@ -0,0 +1,15 @@
+var a = 100;
+a = 123;
+
+if (a === 123) {
+  console.log("cimdoos");
+}
+if (!data || Object.keys(data).length === 0) {
+  console.log("购物党隐私说明（键入disable_gwd_privacy()不再显示本声明）：");
+  console.log(
+    "    我们不会收集用户个人隐私信息，只会收集一些简单的产品交互行为，帮助改善产品，例如记录某个模块点击次数，更多信息请关注："
+  );
+  console.log(
+    "隐私协议: https://www.gwdang.com/static_page/privacy_policy.html"
+  );
+}
